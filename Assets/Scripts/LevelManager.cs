597ae9a53f274f89dda7975ea692e5e9d5b0746a
IGORHOME\Igor <sartoris.flame@gmail.com>
2018-07-16 21:08:11 +0900

    Added splash screen and level manager script

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter); 
+    }
+
+    public void LoadLevel(string _name)
+    {
+        Debug.Log("Load level");
+        SceneManager.LoadScene(_name);
+
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quiting");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+    }  
+ 
+}
